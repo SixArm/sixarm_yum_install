a4426e2ecfb689d97c851fcfa8cedd87f93afeb3
Joel Parker Henderson <joel@joelparkerhenderson.com>
2018-12-18 18:57:17 -0800

    Add builders

--- yum-install-favorite-commands.sh
+++ yum-install-favorite-commands.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+sudo yum install -y automake               `# tool to automatically generate Makefile files`
 sudo yum install -y autoconf               `# automatic configure script builder`
 sudo yum install -y bison                  `# A parser generator that is compatible with YACC`
 sudo yum install -y byacc                  `# public domain Berkeley LALR Yacc parser generator`
@@ -12,6 +13,7 @@ sudo yum install -y elinks                 `# advanced text-mode WWW browser`
 sudo yum install -y flac                   `# Free Lossless Audio Codec - command line tools`
 sudo yum install -y fop                    `# XML to PDF Translator`
 sudo yum install -y gettext                `# GNU Internationalization utilities`
+sudo yum install -y gcc-c++                `# GNU Compiler Collection for C++ code`
 sudo yum install -y gnupg                  `# GNU privacy guard - a free PGP replacement`
 sudo yum install -y gnuplot                `# Gnuplot - A command-line driven interactive plotting program`
 sudo yum install -y gnuplot-doc            `# Gnuplot - A command-line driven interactive plotting program - documentation`
@@ -22,8 +24,9 @@ sudo yum install -y htop                   `# interactive processes viewer`
 sudo yum install -y intltool               `# Utility scripts for internationalizing XML`
 sudo yum install -y iproute                `# networking and traffic control tools`
 sudo yum install -y less                   `# pager program similar to more`
-sudo yum install -y less                   `# pager program similar to more`
 sudo yum install -y libtool                `# GNU generic library support script for shared libraries`
+sudo yum install -y libxml2                `# XML C parser and toolkit`
+sudo yum install -y libxml2-devel          `# XML C parser and toolkit - development files`
 sudo yum install -y lsb                    `# Linux Standard Base 4.0 support package`
 sudo yum install -y lynx                   `# Text-mode WWW Browser (transitional package)`
 sudo yum install -y make                   `# A utility for directing compilation`
@@ -33,10 +36,11 @@ sudo yum install -y mutt                   `# text-based mailreader supporting M
 sudo yum install -y nano                   `# Small friendly text editor inspired by Pico`
 sudo yum install -y ncurses-base           `# ncurses - Basic terminal type definitions`
 sudo yum install -y ncurses-term           `# ncurses - Additional terminal type definitions`
+sudo yum install -y openssl                `# open source Secure Sockets Layer (SSL) protocols`
 sudo yum install -y patch                  `# Apply a diff file to an original`
 sudo yum install -y rsync                  `# fast remote file copy program (like rcp)`
 sudo yum install -y screen                 `# terminal multiplexor with VT100/ANSI terminal emulation`
-sudo yum install -y sysfsutils             `# sysfs query tool and boot-time setup`
+sudo yum install -y stunnel                `# secure encrypted tunneling service`
 sudo yum install -y sysfsutils             `# sysfs query tool and boot-time setup`
 sudo yum install -y tcsh                   `# TENEX C Shell, an enhanced version of Berkeley csh`
 sudo yum install -y telnet                 `# The telnet client`
@@ -46,4 +50,5 @@ sudo yum install -y udev                   `# rule-based device node and kernel 
 sudo yum install -y unzip                  `# De-archiver for .zip files`
 sudo yum install -y uuid                   `# Universally Unique Identifier command-line tool`
 sudo yum install -y xauth                  `# X authentication utility`
+sudo yum install -y zlib-devel             `# data compression library`
 sudo yum install -y zsh                    `# A shell with lots of features`
